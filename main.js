3818e067920c90f5a37ce8f9e7a01bf18db07db3
edouardduprat <edouard421@gmail.com>
2019-01-30 15:46:06 -0800

    fader

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-window.addEventListener("scroll", function() {showFunction()});
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 
-function showFunction() {
-    if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
-        document.getElementById("toptexts2").style.display = "block";
-    } else {
-        document.getElementById("toptexts2").style.display = "none";
-    } 
+
+
+
+   }else
+   {
+    window.addEventListener("scroll", function() {showFunction()});
+
+    function showFunction() {
+        if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
+            document.getElementById("fader2").style.display = "block";
+        } else {
+            document.getElementById("fader2").style.display = "none";
+        } 
+    }
 }
