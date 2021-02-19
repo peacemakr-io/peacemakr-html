63612d953219c99cbe5f31f3f278be953367e5ff
Fredstem <Fredstem@users.noreply.github.com>
2021-02-19 22:05:43 +0000

    (GH actions bot) Build production website

--- docs/src.9f7dedc1.js
+++ docs/src.9f7dedc1.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./styles.css");
 },{"./styles.css":"Tnu0"}]},{},["Focm"], "navbar.js")
-//# sourceMappingURL=/src.aa5b42a1.js.map
+//# sourceMappingURL=/src.9f7dedc1.js.map
