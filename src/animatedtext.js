27998dc131babc5ff9473e3a619073d756a68d4a
Jon McLachlan <jon.mclachlan@icloud.com>
2021-03-07 19:29:47 -0800

    Edits from everyone

--- src/animatedtext.js
+++ src/animatedtext.js
@@ -65,7 +65,7 @@ var TxtType = function(el, toRotate, period) {
         this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
         var that = this;
-        var delta = 50 - Math.random() * 50;
+        var delta = 25;
 
         if (this.isDeleting) { delta /= 2; }
 
