df47441a1bc64be9e0b6cd89cb61b4ce39dec997
interstellarPotato <sasha.sinkevich@gmail.com>
2021-03-28 22:45:41 -0700

    add content to the faq page

--- src/navbar.js
+++ src/navbar.js
@@ -72,7 +72,7 @@ class NavigationBar extends HTMLElement {
                                     </div>
                                 </a>
 
-                                <a href="#" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
+                                <a href="engineers_faqs.html" class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
                                     <!-- Heroicon name: outline/view-grid -->
                                     <svg class="flex-shrink-0 h-6 w-6 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
@@ -188,7 +188,7 @@ class NavigationBar extends HTMLElement {
                         SDKs
                     </a>
 
-                    <a href="#" class="text-base font-medium text-gray-900 hover:text-gray-700">
+                    <a href="engineers_faqs.html" class="text-base font-medium text-gray-900 hover:text-gray-700">
                         FAQs
                     </a>
 
